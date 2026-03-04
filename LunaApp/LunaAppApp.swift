373d1ebde482d91c1220953d6b0036f52d25e783
BeerHobbit <kovalenko537@gmail.com>
2026-03-04 04:47:15 +0300

    add xcode project

---
+++ LunaApp/LunaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaAppApp.swift
+//  LunaApp
+//
+//  Created by Артём Коваленко on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LunaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
